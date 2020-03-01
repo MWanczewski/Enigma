ffdd9fc98d44b94ac72d746afae2eae02efe62a9
MWanczewski <marekwanczewski@gmail.com>
2020-03-01 17:04:35 +0100

    Task-9. ser model added.

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,23 +2,23 @@ package controllers;
 
 import application.Main;
 import ciphers.Cipher;
-import ciphers.impl.CesarCipher;
 import ciphers.impl.VigenereCipher;
 import factories.CipherFactory;
 import factories.impl.CipherFactoryImpl;
 import file.utils.FileTool;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Optional;
@@ -74,13 +74,13 @@ public class MainController implements Initializable {
         if (!userText.isEmpty()) {
             String cipherType = cipherChoiceBox.getValue();
             Cipher cipher = cipherFactory.create(cipherType);
-            if(cipher instanceof VigenereCipher) {
+            if (cipher instanceof VigenereCipher) {
                 TextInputDialog inputDialog = new TextInputDialog("Key");
                 inputDialog.setContentText("Key: ");
                 inputDialog.setHeaderText("If you know the key, write it here.");
                 Optional<String> userInputOptional = inputDialog.showAndWait();
-                if(userInputOptional.isPresent() && !userInputOptional.get().equals("")) {
-                    ((VigenereCipher)(cipher)).setKey(userInputOptional.get());
+                if (userInputOptional.isPresent() && !userInputOptional.get().equals("")) {
+                    ((VigenereCipher) (cipher)).setKey(userInputOptional.get());
                 } else {
                     return;
                 }
@@ -118,5 +118,24 @@ public class MainController implements Initializable {
             String fileContent = FileTool.getFileContent(fileToLoad.getAbsolutePath());
             inputTextArea.setText(fileContent);
         }
+    }
+
+    @FXML
+    public void registerUser() {
+        Alert addUserAlert = new Alert(Alert.AlertType.INFORMATION);
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/RegisterUser.fxml"));
+        try {
+            AnchorPane userLayout = loader.load();
+            addUserAlert.getDialogPane().setContent(userLayout);
+            addUserAlert.showAndWait();
+        } catch (IOException e) {
+            System.out.println("Could not load userLayout because of " + e);
+        }
+    }
+
+    @FXML
+    public void closeApp() {
+        Platform.exit();
+        System.exit(0);
     }
 }
